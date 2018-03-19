918a115b892740d4ed1ed2cf0b8df6796513b94a
claireb25 <claire.bourgeois.cb@gmail.com>
2018-03-19 13:10:05 +0100

    ajout image

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var carte = [12,12];
 var x = 0;
 var y = 0;
-const sol = document.getElementById('fond');
+
+
 console.log(sol);
 for (x = 0; x<12; x++){
     for (y = 0; y<12; y++){
+        var carreSol = document.createElement("div");
+        divsetAttribute
         sol.style.top = y*40 + 'px';
         sol.style.left = x*40 + 'px';
-        sol.src = "media/sol.jpg"  
-        console.log(sol.style.top);
     }
    
 }
