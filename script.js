811cf04fc20f79cb5b94ea647280b04224a54516
NJacq <nicolas.j@codeur.online>
2018-03-19 17:59:11 +0100

    ajout depot bombe

--- script.js
+++ script.js
@@ -54,8 +54,30 @@ Fin partie Claire***/
 
 
 /*Debut partie Nicolas**/
-var 
-function bombset()
+var hero = document.getElementById('bomberMan');
+var hauteurHero = hero.offsetLeft;
+console.log(hero.offsetLeft)
+var largeurHero = hero.offsetTop;
+console.log(hero.offsetTop)
+var positionVerticale = largeurHero/40;
+console.log(positionVerticale)
+var positionHorizontale = hauteurHero/40;
+console.log(positionHorizontale)
+var bomb = document.getElementById('bomb');
+document.addEventListener('keydown', function bombset(evenement){
+
+
+      if (evenement.keyCode == 32){
+          console.log("coucou")
+    bomb.style.backgroundColor = "red";
+    console.log(bomb.style.backgroundColor)
+    bomb.style.top = positionVerticale*40+ 'px';
+    console.log(bomb.style.top)
+    bomb.style.left = positionHorizontale*40+ 'px';
+    console.log(bomb.style.left)
+         }  })
+    
+    
 
 
 /*Fin partie Nicolas/*
