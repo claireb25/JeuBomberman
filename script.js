c7e90cd1262243797fb259799c78e599cbdb982c
Tom Martin <tommartin@MacBook-Pro-de-Tom.local>
2018-03-19 14:59:18 +0100

    recuperation valeur tableau

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 var carte = [
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0]
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0]
 ];
 var x = 0;
 var y = 0;
 var carreSol;
+var carreMur;
 var cadre = document.getElementById('fenetre');
 
 for (x=0; x<12; x++){
     for (y=0; y<12; y++){
+       if (carte[y][x] == 0)
+       {
         carreSol = document.createElement("div");
         carreSol.setAttribute("class","sol");
         cadre.appendChild(carreSol);
         carreSol.style.top = y*40 + 'px';
         carreSol.style.left = x*40 + 'px';
+       }
         
-        
-        console.log(carreSol.offsetTop);
-        
+       
+       else if (carte[y][x] == 0)
+       {
+        carreMur = document.createElement("div");
+        carreMur.setAttribute("class","mur");
+        cadre.appendChild(carreSol);
+        carreMur.style.top = y*40 + 'px';
+        carreMur.style.left = x*40 + 'px';
+       }
         
         
     }
