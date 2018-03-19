ef43d1ce843d228ce13d91107f39a9eaddf00cfa
claireb25 <claire.bourgeois.cb@gmail.com>
2018-03-19 13:46:28 +0100

    ajout fonctionalité création de div class sol

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 var carte = [12,12];
 var x = 0;
 var y = 0;
+var carreSol;
+var cadre = document.getElementById('fenetre');
 
-
-console.log(sol);
-for (x = 0; x<12; x++){
-    for (y = 0; y<12; y++){
-        var carreSol = document.createElement("div");
-        divsetAttribute
-        sol.style.top = y*40 + 'px';
-        sol.style.left = x*40 + 'px';
+for (x=0; x<12; x++){
+    for (y=0; y<12; y++){
+        carreSol = document.createElement("div");
+        carreSol.setAttribute("class","sol");
+        cadre.appendChild(carreSol);
+        carreSol.style.top = y*40 + 'px';
+        carreSol.style.left = x*40 + 'px';
+        
+        
+        console.log(carreSol.offsetTop);
+        
+        
+        
     }
    
 }
